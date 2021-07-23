4bdfca4b2ce87b9d4e7c695dd686065ee480fad2
nhnujhat <nhnujhat@gmail.com>
2021-07-24 01:32:48 +0600

    Pedometer fixed

--- app/src/main/java/com/example/thrive/Pedometer.java
+++ app/src/main/java/com/example/thrive/Pedometer.java
@@ -22,91 +22,39 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class Pedometer extends AppCompatActivity{
+public class Pedometer extends AppCompatActivity implements SensorEventListener{
 
-    private TextView textViewStepCounter;
-    private SensorManager sensorManager;
-    private Sensor mStepCounter;
+    TextView textViewStepCounter;
+    SensorManager sensorManager;
+    boolean running = false;
 
-    //private boolean isCounterSensorPresent;
-
-    private int stepCount = 0;
-    private double magnitudePrevious = 0;
-
-    //private int SENSOR_PERMISSION_CODE = 1;
-    //Button permissionbutton;
+    private int SENSOR_PERMISSION_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pedometer);
 
-        /*permissionbutton=(Button)findViewById(R.id.permissionbutton);
-        permissionbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(ContextCompat.checkSelfPermission(Pedometer.this,
-                        Manifest.permission.BODY_SENSORS) == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(Pedometer.this,"You Have Already Granted This Permission!",Toast.LENGTH_SHORT).show();
-                } else {
-                    requestBodySensorPermission();
-                }
-            }
-        });*/
+        if(ContextCompat.checkSelfPermission(Pedometer.this,
+                Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_GRANTED) {
 
-        //getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        } else {
+            requestActivitySensorPermission();
+        }
+
         textViewStepCounter = findViewById(R.id.textViewStepCounter);
-
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        mStepCounter = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-
-        SensorEventListener stepDetector = new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent sensorEvent) {
-                if(sensorEvent != null) {
-                    float x_acceleration = sensorEvent.values[0];
-                    float y_acceleration = sensorEvent.values[1];
-                    float z_acceleration = sensorEvent.values[2];
-
-                    double magnitude = Math.sqrt(x_acceleration*x_acceleration+y_acceleration*y_acceleration+z_acceleration*z_acceleration);
-                    double magnitudeDelta = magnitude - magnitudePrevious;
-
-                    magnitudePrevious = magnitude;
-
-                    if( magnitudeDelta > 6) {
-                        stepCount++;
-                    }
-                    textViewStepCounter.setText(String.valueOf(stepCount));
-                }
-
-            }
-
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int i) {
-
-            }
-        };
-
-        sensorManager.registerListener(stepDetector,mStepCounter, SensorManager.SENSOR_DELAY_NORMAL);
-
-        /*if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null) {
-            mStepCounter = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-            isCounterSensorPresent = true;
-        }else
-        {
-            textViewStepCounter.setText("Counter Sensor is Not Present");
-            isCounterSensorPresent = false;
-        }*/
     }
-    /*private void requestBodySensorPermission() {
-        if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.BODY_SENSORS)) {
+
+    private void requestActivitySensorPermission() {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACTIVITY_RECOGNITION)) {
             new AlertDialog.Builder(this)
                     .setTitle("permission needed")
                     .setMessage("this permission is needed to count your steps")
                     .setPositiveButton("ok", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
-                            ActivityCompat.requestPermissions(Pedometer.this, new String[] {Manifest.permission.BODY_SENSORS}, SENSOR_PERMISSION_CODE);
+                            ActivityCompat.requestPermissions(Pedometer.this, new String[] {Manifest.permission.ACTIVITY_RECOGNITION}, SENSOR_PERMISSION_CODE);
                         }
                     })
                     .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
@@ -117,7 +65,7 @@ public class Pedometer extends AppCompatActivity{
                     })
                     .create().show();
         } else {
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.BODY_SENSORS}, SENSOR_PERMISSION_CODE);
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACTIVITY_RECOGNITION}, SENSOR_PERMISSION_CODE);
         }
     }
 
@@ -130,59 +78,36 @@ public class Pedometer extends AppCompatActivity{
                 Toast.makeText(this, "Permission Denied",Toast.LENGTH_SHORT).show();
             }
         }
-    }*/
+    }
 
-    /*@Override
+    @Override
+    protected void onResume() {
+        super.onResume();
+        running = true;
+        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if(countSensor != null) {
+            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
+        } else {
+            Toast.makeText(this, "Sensor not found!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        running = false;
+
+    }
+
+    @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        if(sensorEvent.sensor == mStepCounter) {
-            stepCount = (int) sensorEvent.values[0];
-            textViewStepCounter.setText(String.valueOf(stepCount));
+        if(running) {
+            textViewStepCounter.setText(String.valueOf(sensorEvent.values[0]));
         }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null)
-            sensorManager.registerListener(this,mStepCounter,SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null)
-            sensorManager.unregisterListener(this,mStepCounter);
-    }*/
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.clear();
-        editor.putInt("stepCount", stepCount);
-        editor.apply();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.clear();
-        editor.putInt("stepCount", stepCount);
-        editor.apply();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
-        stepCount = sharedPreferences.getInt("stepCount", 0);
     }
 }
