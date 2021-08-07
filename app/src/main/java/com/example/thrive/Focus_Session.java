74a8f4debd640b2d4de2f7354afb180c0d1d89ee
WrightlyRong <tasmia719@gmail.com>
2021-08-07 19:52:15 +0600

    Added Alarm to Focus_Session and changed some button features

--- app/src/main/java/com/example/thrive/Focus_Session.java
+++ app/src/main/java/com/example/thrive/Focus_Session.java
@@ -2,10 +2,14 @@ package com.example.thrive;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.SharedPreferences;
+import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,51 +30,52 @@ public class Focus_Session extends AppCompatActivity {
     private boolean timerRunning;
     private long timeLeftInMillis;
     private long endTime;
-    FloatingActionButton startbtn,pausebtn,resetbtn;
+    FloatingActionButton startbtn, pausebtn, resetbtn;
+
+    Dialog alarmDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_focus__session);
 
-        timetext=findViewById(R.id.timerminutetext);
-        setTime=findViewById(R.id.settimerButton);
+        alarmDialog = new Dialog(Focus_Session.this);
+
+        timetext = findViewById(R.id.timerminutetext);
+        setTime = findViewById(R.id.settimerButton);
         setTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-             String input = timetext.getText().toString();
-             if(input.length()==0)
-             {
-                 Toast.makeText(Focus_Session.this,"Enter a number", Toast.LENGTH_SHORT).show();
-                 return;
-             }
-             long millisInput= Long.parseLong(input)*60000;
-             if(millisInput==0){
-                 Toast.makeText(Focus_Session.this,"Enter a positive number", Toast.LENGTH_SHORT).show();
-                 return;
-             }
-             setTimer(millisInput);
-             timetext.setText("");
+                String input = timetext.getText().toString();
+                if (input.length() == 0) {
+                    Toast.makeText(Focus_Session.this, "Enter a number", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                long millisInput = Long.parseLong(input) * 60000;
+                if (millisInput == 0) {
+                    Toast.makeText(Focus_Session.this, "Enter a positive number", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                setTimer(millisInput);
+                timetext.setText("");
 
             }
         });
-        textViewcountdown=findViewById(R.id.countdown);
+        textViewcountdown = findViewById(R.id.countdown);
         startbtn = findViewById(R.id.fab_start);
         startbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!timerRunning)
-                {
+                if (!timerRunning) {
                     startTimer();
                 }
             }
         });
-         pausebtn = findViewById(R.id.fab_pause);
+        pausebtn = findViewById(R.id.fab_pause);
         pausebtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(timerRunning)
-                {
+                if (timerRunning) {
                     pauseTimer();
                 }
 
@@ -83,58 +88,60 @@ public class Focus_Session extends AppCompatActivity {
                 resetTimer();
             }
         });
-
     }
 
-    private void setTimer(long milliseconds){
-      startTimeInMillies=milliseconds;
-      resetTimer();
-      closeKeyboard();
+    private void setTimer(long milliseconds) {
+        startTimeInMillies = milliseconds;
+        resetTimer();
+        closeKeyboard();
 
     }
 
     private void startTimer() {
-        endTime=System.currentTimeMillis()+timeLeftInMillis;
+        endTime = System.currentTimeMillis() + timeLeftInMillis;
         countDownTimer = new CountDownTimer(timeLeftInMillis, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
-             timeLeftInMillis=millisUntilFinished;
-             updateCountDownText();
+                timeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
             }
 
             @Override
             public void onFinish() {
-              timerRunning=false;
+                timerRunning = false;
+                final MediaPlayer mediaPlayer = MediaPlayer.create(Focus_Session.this, R.raw.samsung_relax);
+                mediaPlayer.start();
+                dismissAlarm(mediaPlayer);
+                alarmDialog.show();
             }
         }.start();
 
-        timerRunning=true;
+        timerRunning = true;
     }
 
     private void pauseTimer() {
         countDownTimer.cancel();
-        timerRunning=false;
+        timerRunning = false;
     }
 
     private void resetTimer() {
-        if(countDownTimer!=null) {
+        if (countDownTimer != null) {
             countDownTimer.cancel();
         }
-        timerRunning=false;
-        timeLeftInMillis= startTimeInMillies;
+        timerRunning = false;
+        timeLeftInMillis = startTimeInMillies;
         updateCountDownText();
     }
 
     private void updateCountDownText() {
-        int hours= (int) (timeLeftInMillis/1000)/3600;
-        int minutes= (int) ((timeLeftInMillis/1000)%3600)/60;
-        int seconds= (int) (timeLeftInMillis/1000)%60;
+        int hours = (int) (timeLeftInMillis / 1000) / 3600;
+        int minutes = (int) ((timeLeftInMillis / 1000) % 3600) / 60;
+        int seconds = (int) (timeLeftInMillis / 1000) % 60;
         String timeLeftFormatted;
-        if(hours>0){
-            timeLeftFormatted = String.format(Locale.getDefault(),"%d:%02d:%02d",hours,minutes,seconds);
-        }
-        else{
-        timeLeftFormatted = String.format(Locale.getDefault(),"%02d:%02d",minutes,seconds);
+        if (hours > 0) {
+            timeLeftFormatted = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, seconds);
+        } else {
+            timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
         }
         textViewcountdown.setText(timeLeftFormatted);
     }
@@ -166,45 +173,74 @@ public class Focus_Session extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        SharedPreferences preferences= getSharedPreferences("preferences", MODE_PRIVATE);
-        timeLeftInMillis=preferences.getLong("millisLeft", startTimeInMillies);
-        timerRunning= preferences.getBoolean("timerRunning", false);
-        startTimeInMillies= preferences.getLong("startTimeInMillis",600000);
+        SharedPreferences preferences = getSharedPreferences("preferences", MODE_PRIVATE);
+        timeLeftInMillis = preferences.getLong("millisLeft", startTimeInMillies);
+        timerRunning = preferences.getBoolean("timerRunning", false);
+        startTimeInMillies = preferences.getLong("startTimeInMillis", 600000);
         updateCountDownText();
-        if(timerRunning){
-            endTime=preferences.getLong("endTime", 0);
-            timeLeftInMillis=endTime-System.currentTimeMillis();
-            if(timeLeftInMillis<0){
-                timeLeftInMillis=0;
-                timerRunning=false;
+        if (timerRunning) {
+            endTime = preferences.getLong("endTime", 0);
+            timeLeftInMillis = endTime - System.currentTimeMillis();
+            if (timeLeftInMillis < 0) {
+                timeLeftInMillis = 0;
+                timerRunning = false;
                 updateCountDownText();
+            } else {
+                startTimer();
             }
-            else
-            {startTimer();}
         }
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        SharedPreferences preferences= getSharedPreferences("preferences", MODE_PRIVATE);
-        SharedPreferences.Editor editor= preferences.edit();
+        SharedPreferences preferences = getSharedPreferences("preferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
         editor.putLong("startTimeInMillis", startTimeInMillies);
         editor.putLong("millisLeft", timeLeftInMillis);
         editor.putBoolean("timerRunning", timerRunning);
         editor.putLong("endTime", endTime);
         editor.apply();
-        if(countDownTimer!=null) {
+        if (countDownTimer != null) {
             countDownTimer.cancel();
         }
     }
 
-    private void closeKeyboard()
-    {
-        View view= this.getCurrentFocus();
-        if(view!=null) {
-            InputMethodManager inputMethodManager= (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-            inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(),0);
-        }
+    private void closeKeyboard() {
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
         }
     }
+
+    private void dismissAlarm(MediaPlayer mediaPlayer) {
+
+        alarmDialog.setContentView(R.layout.alarm_dialog);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            alarmDialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.alarm_dialog_background));
+        }
+        alarmDialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        alarmDialog.setCancelable(false);
+
+        Button yes_turnOffAlarm = alarmDialog.findViewById(R.id.buttonAlarmYes);
+        Button no_turnOffAlarm = alarmDialog.findViewById(R.id.buttonAlarmNo);
+
+        yes_turnOffAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.stop();
+                Toast.makeText(Focus_Session.this, "Turned Off", Toast.LENGTH_SHORT).show();
+                alarmDialog.dismiss();
+            }
+        });
+
+        no_turnOffAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Focus_Session.this, "Not Turned Off", Toast.LENGTH_SHORT).show();
+                alarmDialog.dismiss();
+            }
+        });
+    }
+}
