2ec2d9536165c106207df074d47e2f8c557c026d
nhnujhat <nhnujhat@gmail.com>
2021-08-19 15:37:36 +0600

    Add goals

--- app/src/main/java/com/example/thrive/AddGoalsHealth.java
+++ app/src/main/java/com/example/thrive/AddGoalsHealth.java
@@ -6,16 +6,39 @@ import androidx.appcompat.widget.SwitchCompat;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AddGoalsHealth extends AppCompatActivity {
 
+    public static final String TAG = "TAG AddGoal";
     SwitchCompat switchCompat;
     Button waterIntakebtn, medIntakebtn, workoutbtn, yogabtn, foodhabitbtn, sportbtn, sleepbtn, rehabilitationbrn,bodycarebtn;
     Button reminderbtn;
+    EditText goalname, goalduration;
+    Button setgoal;
+    Button gopublic, later;
+
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userID;
+    String category;
+    String privacy="";
+
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +55,18 @@ public class AddGoalsHealth extends AppCompatActivity {
         rehabilitationbrn=findViewById(R.id.rehabilitationbtn);
         bodycarebtn=findViewById(R.id.bodycarebtn);
         reminderbtn=findViewById(R.id.reminderbtn);
+
+        goalname=findViewById(R.id.goalName);
+        goalduration=findViewById(R.id.goalDuration);
+        setgoal=findViewById(R.id.setgoalbtn);
+
+        gopublic=findViewById(R.id.publicbtn);
+        later=findViewById(R.id.laterbtn);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+        userID = fAuth.getCurrentUser().getUid();
+
         reminderbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,6 +79,7 @@ public class AddGoalsHealth extends AppCompatActivity {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 waterIntakebtn.setPressed(true);
+                category="Water Intake";
                 medIntakebtn.setPressed(false);
                 yogabtn.setPressed(false);
                 sportbtn.setPressed(false);
@@ -61,6 +97,7 @@ public class AddGoalsHealth extends AppCompatActivity {
             public boolean onTouch(View v, MotionEvent event) {
                 waterIntakebtn.setPressed(false);
                 medIntakebtn.setPressed(true);
+                category="Medicine Intake";
                 yogabtn.setPressed(false);
                 sportbtn.setPressed(false);
                 sleepbtn.setPressed(false);
@@ -78,6 +115,7 @@ public class AddGoalsHealth extends AppCompatActivity {
                 waterIntakebtn.setPressed(false);
                 medIntakebtn.setPressed(false);
                 yogabtn.setPressed(true);
+                category="Yoga";
                 sportbtn.setPressed(false);
                 sleepbtn.setPressed(false);
                 rehabilitationbrn.setPressed(false);
@@ -95,6 +133,7 @@ public class AddGoalsHealth extends AppCompatActivity {
                 medIntakebtn.setPressed(false);
                 yogabtn.setPressed(false);
                 sportbtn.setPressed(true);
+                category="Sports";
                 sleepbtn.setPressed(false);
                 rehabilitationbrn.setPressed(false);
                 bodycarebtn.setPressed(false);
@@ -112,6 +151,7 @@ public class AddGoalsHealth extends AppCompatActivity {
                 yogabtn.setPressed(false);
                 sportbtn.setPressed(false);
                 sleepbtn.setPressed(true);
+                category="Sleep";
                 rehabilitationbrn.setPressed(false);
                 bodycarebtn.setPressed(false);
                 workoutbtn.setPressed(false);
@@ -129,6 +169,7 @@ public class AddGoalsHealth extends AppCompatActivity {
                 sportbtn.setPressed(false);
                 sleepbtn.setPressed(false);
                 rehabilitationbrn.setPressed(true);
+                category="Rehabilitation";
                 bodycarebtn.setPressed(false);
                 workoutbtn.setPressed(false);
                 foodhabitbtn.setPressed(false);
@@ -146,6 +187,7 @@ public class AddGoalsHealth extends AppCompatActivity {
                 sleepbtn.setPressed(false);
                 rehabilitationbrn.setPressed(false);
                 bodycarebtn.setPressed(true);
+                category="Body Care";
                 workoutbtn.setPressed(false);
                 foodhabitbtn.setPressed(false);
                 return true;
@@ -162,6 +204,7 @@ public class AddGoalsHealth extends AppCompatActivity {
                 rehabilitationbrn.setPressed(false);
                 bodycarebtn.setPressed(false);
                 workoutbtn.setPressed(true);
+                category="Working Out";
                 foodhabitbtn.setPressed(false);
                 return true;
             }
@@ -178,10 +221,75 @@ public class AddGoalsHealth extends AppCompatActivity {
                 bodycarebtn.setPressed(false);
                 workoutbtn.setPressed(false);
                 foodhabitbtn.setPressed(true);
+                category="Food Habit";
                 return true;
             }
         });
 
+        gopublic.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                gopublic.setPressed(true);
+                privacy="Public";
+                later.setPressed(false);
+                return true;
+            }
+        });
+        later.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                gopublic.setPressed(false);
+                later.setPressed(true);
+                privacy="";
+                return true;
+            }
+        });
+
+        setgoal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = goalname.getText().toString();
+                String duration = goalduration.getText().toString();
+                if (name.isEmpty()) {
+                    showError(goalname, "Name must not be empty");
+                    return;
+                }
+                if (duration.isEmpty() || duration.equals("0")) {
+                    showError(goalduration, "Duration must be at least 1 day");
+                    return;
+                }
+                DocumentReference documentReference1 = fStore.collection("UserGoalInfo").document(userID).collection("Goals").document();
+                Map<String,Object> goal = new HashMap<>();
+                goal.put("Name", name);
+                goal.put("Category", "Health");
+                goal.put("Subcategory", category);
+                goal.put("Duration", duration);
+                goal.put("Privacy",privacy);
+
+                documentReference1.set(goal).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "onSuccess: goal is created");
+                    }
+
+                });
+                Toast.makeText(AddGoalsHealth.this, "Goal Added", Toast.LENGTH_SHORT).show();
+
+                goalname.setText("");
+                goalduration.setText("");
+                waterIntakebtn.setPressed(false);
+                medIntakebtn.setPressed(false);
+                yogabtn.setPressed(false);
+                sportbtn.setPressed(false);
+                sleepbtn.setPressed(false);
+                rehabilitationbrn.setPressed(false);
+                bodycarebtn.setPressed(false);
+                workoutbtn.setPressed(false);
+                foodhabitbtn.setPressed(false);
+                gopublic.setPressed(false);
+                later.setPressed(false);
+            }
+        });
 
         switchCompat.setChecked(false);
         switchCompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -198,6 +306,11 @@ public class AddGoalsHealth extends AppCompatActivity {
              }
             }
         });
+    }
+
+    private void showError(EditText input, String s) {
+        input.setError(s);
+        input.requestFocus();
     }
 
     @Override
--- app/src/main/java/com/example/thrive/AddGoalsProductivity.java
+++ app/src/main/java/com/example/thrive/AddGoalsProductivity.java
@@ -3,17 +3,40 @@ package com.example.thrive;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AddGoalsProductivity extends AppCompatActivity {
+    public static final String TAG = "TAG AddGoal";
     SwitchCompat switchCompat;
     Button studybtn, hobbybtn, softbtn, techbtn,reminderbtn;
+    EditText goalname, goalduration;
+    Button setgoal;
+    Button gopublic, later;
 
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userID;
+    String category;
+    String privacy="";
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +47,18 @@ public class AddGoalsProductivity extends AppCompatActivity {
         softbtn=findViewById(R.id.softskillbtn);
         techbtn=findViewById(R.id.technicalbtn);
         reminderbtn=findViewById(R.id.reminderbtn);
+
+        goalname=findViewById(R.id.goalName);
+        goalduration=findViewById(R.id.goalDuration);
+        setgoal=findViewById(R.id.setgoalbtn);
+
+        gopublic=findViewById(R.id.publicbtn);
+        later=findViewById(R.id.laterbtn);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+        userID = fAuth.getCurrentUser().getUid();
+
         reminderbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,6 +71,7 @@ public class AddGoalsProductivity extends AppCompatActivity {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 studybtn.setPressed(true);
+                category="Study";
                 hobbybtn.setPressed(false);
                 softbtn.setPressed(false);
                 techbtn.setPressed(false);
@@ -48,6 +84,7 @@ public class AddGoalsProductivity extends AppCompatActivity {
             public boolean onTouch(View v, MotionEvent event) {
                 studybtn.setPressed(false);
                 hobbybtn.setPressed(true);
+                category="Hobby";
                 softbtn.setPressed(false);
                 techbtn.setPressed(false);
                 return true;
@@ -60,6 +97,7 @@ public class AddGoalsProductivity extends AppCompatActivity {
                 studybtn.setPressed(false);
                 hobbybtn.setPressed(false);
                 softbtn.setPressed(true);
+                category="Soft Skill Development";
                 techbtn.setPressed(false);
                 return true;
             }
@@ -72,9 +110,72 @@ public class AddGoalsProductivity extends AppCompatActivity {
                 hobbybtn.setPressed(false);
                 softbtn.setPressed(false);
                 techbtn.setPressed(true);
+                category="Technical Skill Development";
                 return true;
             }
         });
+
+        gopublic.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                gopublic.setPressed(true);
+                privacy="Public";
+                later.setPressed(false);
+                return true;
+            }
+        });
+        later.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                gopublic.setPressed(false);
+                privacy="";
+                later.setPressed(true);
+                return true;
+            }
+        });
+
+        setgoal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = goalname.getText().toString();
+                String duration = goalduration.getText().toString();
+                if (name.isEmpty()) {
+                    showError(goalname, "Name must not be empty");
+                    return;
+                }
+                if (duration.isEmpty() || duration.equals("0")) {
+                    showError(goalduration, "Duration must be at least 1 day");
+                    return;
+                }
+                DocumentReference documentReference1 = fStore.collection("UserGoalInfo").document(userID).collection("Goals").document();
+                Map<String,Object> goal = new HashMap<>();
+                goal.put("Name", name);
+                goal.put("Category", "Productivity");
+                goal.put("Subcategory", category);
+                goal.put("Duration", duration);
+                goal.put("Privacy",privacy);
+
+                documentReference1.set(goal).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "onSuccess: goal is created");
+                    }
+
+                });
+
+                Toast.makeText(AddGoalsProductivity.this, "Goal Added", Toast.LENGTH_SHORT).show();
+
+                goalname.setText("");
+                goalduration.setText("");
+                studybtn.setPressed(false);
+                hobbybtn.setPressed(false);
+                softbtn.setPressed(false);
+                techbtn.setPressed(false);
+                gopublic.setPressed(false);
+                later.setPressed(false);
+            }
+        });
+
         switchCompat.setChecked(true);
         switchCompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -89,6 +190,11 @@ public class AddGoalsProductivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void showError(EditText input, String s) {
+        input.setError(s);
+        input.requestFocus();
     }
 
     @Override
